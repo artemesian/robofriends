35d7415a28ecda55030d3f09f216bc48583d8548
angeloDiepe <angelodiepe10@yahoo.com>
2019-10-30 19:02:38 +0100

    Updates

---
+++ precache-manifest.47c08a0c3783e06f4d49a845aa9ff884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7500f2453c7cdb8e3dd78af478a2f7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "477e081aef78fdc53aaa",
+    "url": "/robofriends/static/css/main.d9174270.chunk.css"
+  },
+  {
+    "revision": "9189a2123dd6c43fc7a2",
+    "url": "/robofriends/static/js/2.f2654d2d.chunk.js"
+  },
+  {
+    "revision": "477e081aef78fdc53aaa",
+    "url": "/robofriends/static/js/main.9e38463b.chunk.js"
+  },
+  {
+    "revision": "8aae76b675a2b9367824",
+    "url": "/robofriends/static/js/runtime-main.55754d9f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
